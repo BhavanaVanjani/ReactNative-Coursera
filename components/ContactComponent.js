7b1eecf7fd8376c7be0619c5fd0871fbfce3b012
BhavanaVanjani <bhavana.vanjani@gmail.com>
2019-09-13 11:11:59 +0530

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
-import {View,FlatList,Text,StyleSheet} from 'react-native';
+import {View,FlatList,Text,StyleSheet,ScrollView} from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+
 
 class Contact extends Component {
 
@@ -14,22 +16,26 @@ class Contact extends Component {
 
   render(){
     return(
-        <Card
-            title='Contact Information' >
-          <View>
-            <FlatList
-                data={[
-                  {key:'121, Clear Water Bay Road'},
-                  {key:'Clear Water Bay, Kowloon'},
-                  {key:'HONG KONG'},
-                  {key:'Tel: +852 1234 5678'},
-                  {key:'Fax: +852 8765 4321'},
-                  {key:'Email:confusion@food.net'},
-                ]}
-              renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-              />
-          </View>
-        </Card>
+      <ScrollView>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card
+              title='Contact Information' >
+            <View>
+              <FlatList
+                  data={[
+                    {key:'121, Clear Water Bay Road'},
+                    {key:'Clear Water Bay, Kowloon'},
+                    {key:'HONG KONG'},
+                    {key:'Tel: +852 1234 5678'},
+                    {key:'Fax: +852 8765 4321'},
+                    {key:'Email:confusion@food.net'},
+                  ]}
+                renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                />
+            </View>
+          </Card>
+      </Animatable.View>
+     </ScrollView>
     );
   }
 }
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
 import { postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return{
@@ -24,6 +25,7 @@ function RenderDish(props) {
 
    if(dish != null){
      return(
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
         <Card
             featuredTitle={dish.name}
             image={{ uri: baseUrl + dish.image }}
@@ -50,6 +52,7 @@ function RenderDish(props) {
                   />
             </View>
         </Card>
+      </Animatable.View>
      );
    }
    else{
@@ -71,13 +74,15 @@ function RenderComments(props){
   }
 
   return(
-      <Card title="Comments">
-        <FlatList
-            data={comments}
-            renderItem={renderCommentItem}
-            keyExtractor={item => item.id.toString()}
-            />
-      </Card>
+    <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+        <Card title="Comments">
+          <FlatList
+              data={comments}
+              renderItem={renderCommentItem}
+              keyExtractor={item => item.id.toString()}
+              />
+        </Card>
+    </Animatable.View>
   );
 }
 
